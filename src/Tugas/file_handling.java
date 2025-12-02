34ebef5e5baa602532211b8f58bb4eb65dca0e00
Joshua Simangunsong <youremail@example.com>
2025-12-02 22:24:32 +0700

    Read myfile.txt

--- src/Tugas/file_handling.java
+++ src/Tugas/file_handling.java
@@ -3,21 +3,22 @@ package Tugas;
 
 import  java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class file_handling {
 	
 	public static void main(String[] args) {
 		try {
 			File Obj = new File("myfile.txt");
-			if (Obj.createNewFile()) {
-				System.out.println("File Created = " + Obj.getName());
+			Scanner Reader = new Scanner(Obj);
+			while (Reader.hasNextLine()) {
+				String data = Reader.nextLine();
+				System.out.println(data);
 			}
-			else {
-				System.out.println("File already exist.");
-			}
+			Reader.close();
 		}
-		catch (IOException e) {
-			System.out.println("An error has occurred.");
+		catch (FileNotFoundException e) {
+			System.out.println("AN error has occurred.");
 			e.printStackTrace();
 		}
 	}
